ff9438d9475a034e94494bb0faa3adb0e4a3b58a
lichen777 <lchen139@gmail.com>
2017-11-20 15:17:32 -0600

    bad update, roll back

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,8 +8,8 @@ var game = {
 
     characterItem : ["Name", "HP", "AP", "counterAP", "Img"],
 
-    fighter: [],
-    enemy: [],
+    fighter: undefined,
+    enemy: undefined,
     defender : undefined,
     attackRound : 0,
     APIncrease : 6,
@@ -26,52 +26,52 @@ var game = {
     attack : function() {
         $("#info").empty();
         var HPdamage = (this.fighter[0][2] + (this.APIncrease * this.attackRound))
-    	this.defender[1] -= HPdamage;
-    	this.attackRound ++;
-    	if (this.defender[1] <= 0) {
-    		$("#defender").empty();
-    		this.enemy.splice(this.enemy.indexOf(this.defender),1);
-    		if (typeof this.enemy[0] === "undefined") {
-    			$("button").prop("disabled",true);
-    			$("#display").html("<p>You won! Click to restart!</p>");
-    			$("#display").click(function() {
-    				location.reload();
-				});
-    		}
-            	$("#info").html("<p>" + this.defender[0] + " has been defeated</p>");
-    		this.defender = undefined;
-    		this.lineup(this.enemy, "#enemy");
-    		$(".Img").on("click", function() {
-        		var index = ($(this).attr("data-playerIndex"));
-        		console.log(index);
-        		game.defenderSelect(index);
-       	 	})
-    		//this.attackRound = 0;   // this is a real RPG should have
-    		//this.fighter[0][1] += 50;   // this is a real RPG should have
-    	}else {
-    		this.fighter[0][1] -= this.defender[3];
-    		if (this.fighter[0][1] <= 0){
-    			this.gameover();
-    		}
+        this.defender[1] -= HPdamage;
+        this.attackRound ++;
+        if (this.defender[1] <= 0) {
+            $("#defender").empty();
+            this.enemy.splice(this.enemy.indexOf(this.defender),1);
+            if (typeof this.enemy[0] === "undefined") {
+                $("button").prop("disabled",true);
+                $("#display").html("<p>You won! Click to restart!</p>");
+                $("#display").click(function() {
+                    location.reload();
+                });
+            }
+            $("#info").html("<p>" + this.defender[0] + " has been defeated</p>")
+            this.defender = undefined;
+            game.lineup(game.enemy, "#enemy");
+            $(".Img").on("click", function() {
+                var index = ($(this).attr("data-playerIndex"));
+                console.log(index);
+                game.defenderSelect(index);
+            })
+            //this.attackRound = 0;   // this is a real RPG should have
+            //this.fighter[0][1] += 50;   // this is a real RPG should have
+        }else {
+            this.fighter[0][1] -= this.defender[3];
+            if (this.fighter[0][1] <= 0){
+                this.gameover();
+            }
             $("#info").html("<p>You attacked " + this.defender[0] + " for " + HPdamage + " HP damage.</p><p>" + this.defender[0] + " counter attacked you for " + this.defender[3] + " HP damage.</p>")
-    	}
-    	this.status();
+        }
+        this.status();
     },
     gameover : function() {
-    	$("button").prop("disabled",true);
-    	$("#display").html("<p>You lose! Game Over! Click to restart!</p>");
-    	$("#display").click(function() {
-    		location.reload();
-		});
+        $("button").prop("disabled",true);
+        $("#display").html("<p>You lose! Game Over! Click to restart!</p>");
+        $("#display").click(function() {
+            location.reload();
+        });
     },
     lineup : function(group, place) {
-    	for (var i = 0; i < group.length; i++) {
-    		var imgChar = $("<img>").addClass("Img").attr("src", group[i][4]).attr("data-playerIndex", i) .css("width", "25%");
-    		$(place).append(imgChar);
-    	}
+        for (var i = 0; i < group.length; i++) {
+            var imgChar = $("<img>").addClass("Img").attr("src", group[i][4]).attr("data-playerIndex", i) .css("width", "25%");
+            $(place).append(imgChar);
+        }
     },
     status : function() {
-    	$("#fighter").html($("<img>").attr("src", game.fighter[0][4]).css("width", "25%"));
+        $("#fighter").html($("<img>").attr("src", game.fighter[0][4]).css("width", "25%"));
         $("#fighter").append("<p>" + game.fighter[0][0] + "</p>");
         $("#fighter").append("<p>HP: " + game.fighter[0][1] + "</p>");
         $("#defender").html($("<img>").attr("src", game.defender[4]).css("width", "25%"));
@@ -95,15 +95,14 @@ $(document).ready(function() {
 
         game.lineup(game.enemy, "#enemy");
 
-	$(".Img").on("click", function() {
-        	var index = ($(this).attr("data-playerIndex"));
-        	//console.log(index);
-        	game.defenderSelect(index);
-    	})
+        $(".Img").on("click", function() {
+            var index = ($(this).attr("data-playerIndex"));
+            //console.log(index);
+            game.defenderSelect(index);
+        })
     })
 
     $("button").on("click", function() {
-	console.log(typeof game.defender);
         if(typeof game.defender === "undefined"){
             $("#info").html("<p>Select an enemy</p>");
         }else {
@@ -112,7 +111,6 @@ $(document).ready(function() {
     })
 
 });
-
 
 
 
